6ad39370a54adf7a2cf5fac8409d83d7395666e3
MICHAEL-DELL728\HEIG-Michael <michael.dasilva@heig-vd.ch>
2020-10-10 10:56:39 +0200

    Script to automatically run the server

---
+++ run_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker stop sti_project
+docker rm -f sti_project
+docker run -ti -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
+docker cp site/. sti_project:/usr/share/nginx/
+docker exec -u root sti_project service nginx start
+docker exec -u root sti_project service php5-fpm start
